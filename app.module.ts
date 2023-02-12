2317bcfb9e7eb9b1b1af45cba901a1564e9d9c86
FarisHrvat <80819846+FarisHrvat@users.noreply.github.com>
2023-02-12 13:18:22 +0100

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,25 @@
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { LabNotebookComponent } from './lab-notebook/lab-notebook.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LabNotebookComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
