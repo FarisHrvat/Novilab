91f30ec27e6233958fb3786b707c31e346734190
FarisHrvat <80819846+FarisHrvat@users.noreply.github.com>
2023-02-12 13:18:56 +0100

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,10 @@
+// app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-lab-notebook></app-lab-notebook>'
+})
+export class AppComponent {
+  title = 'lab-notebook';
+}
